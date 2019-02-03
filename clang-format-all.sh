cce89b04ce8a2ab309c2673a3097b3fd5dd05fdc
eddiemcnally <emcn@gmx.com>
2019-02-03 15:06:30 +0000

    Fixed the clang-format script and added a clang-format style file

--- clang-format-all.sh
+++ clang-format-all.sh
@@ -2,5 +2,5 @@
 #
 # Recursively re-formats *.c and *.h using clang-format, and WebKit style.
 # 
-find ./ -iname *.h -o -iname *.c | xargs clang-format -i -style=WebKit
+find ./ -iname *.h -o -iname *.c | xargs clang-format -i -style=file
 
